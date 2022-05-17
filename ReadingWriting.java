72b06b530da0f6a2b20548e89763282df757c752
vishalchhadekar <vishalchhadekar95@gmail.com>
2022-05-17 22:51:34 +0530

    this is file system of lesson3

---
+++ ReadingWriting.java
@@ -0,0 +1,8 @@
+package fileHandling;
+
+public class ReadingWriting {
+	public static void main(String args[]) {
+		System.out.println("This is it");
+	}
+
+}
--- package-info.java
+++ package-info.java
@@ -1,1 +1,1 @@
-package lessionThree.FileHandling;
+package fileHandling;
